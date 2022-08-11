54fd35938a28ca3357b1d1ba3877bb332d40076b
Lucas Veras <lucas.veras01@gmail.com>
2022-08-11 02:07:32 -0300

    Deleted permission package

--- src/main/java/br/com/okfoodsapi/api/controllers/CuisineController.java
+++ src/main/java/br/com/okfoodsapi/api/controllers/CuisineController.java
@@ -3,6 +3,7 @@ package br.com.okfoodsapi.api.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,7 @@ import br.com.okfoodsapi.domain.models.Cuisine;
 import br.com.okfoodsapi.domain.repositories.CuisineRepository;
 
 @RestController
-@RequestMapping("/cuisines")
+@RequestMapping(value = "/cuisines", produces = MediaType.APPLICATION_JSON_VALUE )
 public class CuisineController {
 	
 	@Autowired
