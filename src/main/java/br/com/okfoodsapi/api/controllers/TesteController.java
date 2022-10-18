83100929774115e33c82134e5b9a7f2ec41bd82a
Lucas Rodrigues Veras <lucas.veras01@gmail.com>
2022-10-18 01:32:04 -0300

    Tests keywords JPA

--- src/main/java/br/com/okfoodsapi/api/controllers/TesteController.java
+++ src/main/java/br/com/okfoodsapi/api/controllers/TesteController.java
@@ -1,6 +1,8 @@
 package br.com.okfoodsapi.api.controllers;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.okfoodsapi.domain.models.Cuisine;
+import br.com.okfoodsapi.domain.models.Restaurant;
 import br.com.okfoodsapi.domain.repositories.CuisineRepository;
+import br.com.okfoodsapi.domain.repositories.RestaurantRepository;
 
 @RestController
 @RequestMapping("/teste")
@@ -18,8 +22,28 @@ public class TesteController {
 	@Autowired
 	private CuisineRepository cuisineRepository;
 	
+	@Autowired
+	private RestaurantRepository restaurantRepository;
+	
 	@GetMapping("/cuisines/for-name")
 	public List<Cuisine> cuisineForName(@RequestParam("name") String name){
-		return cuisineRepository.name(name);
+		return cuisineRepository.findByNameContaining(name);
+	}
+	
+	@GetMapping("/cuisines/for-name-single")
+	public Optional<Cuisine> cuisineSingleByName(String name){
+		return cuisineRepository.findSingleByName(name);
+	}
+	
+	@GetMapping("/restaurants/find-tax-shipping-between")
+	public List<Restaurant> findByTaxShippingBetween(
+			BigDecimal taxInit, BigDecimal taxEnd){
+		
+		return restaurantRepository.findByTaxShippingBetween(taxInit, taxEnd);
+	}
+	
+	@GetMapping("/restaurants/for-name")
+	public List<Restaurant> restaurantsForName(String name, Long cuisineId){
+		return restaurantRepository.findByNameContainingAndCuisineId(name, cuisineId);
 	}
 }
