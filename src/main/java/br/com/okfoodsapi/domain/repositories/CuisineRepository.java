d3db067516b77de37ea9b45dbd7d7f35fa2466c0
Lucas Rodrigues Veras <lucas.veras01@gmail.com>
2022-10-18 01:32:04 -0300

    Testes keywords JPA

--- src/main/java/br/com/okfoodsapi/domain/repositories/CuisineRepository.java
+++ src/main/java/br/com/okfoodsapi/domain/repositories/CuisineRepository.java
@@ -1,6 +1,7 @@
 package br.com.okfoodsapi.domain.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,6 +11,8 @@ import br.com.okfoodsapi.domain.models.Cuisine;
 @Repository
 public interface CuisineRepository extends JpaRepository<Cuisine, Long> {
 	
-	List<Cuisine> name(String name);
+	// "Containing" para pesquisar usando like
+	List<Cuisine> findByNameContaining(String name);
 		
+	Optional<Cuisine> findSingleByName(String name);		
 }
--- src/main/java/br/com/okfoodsapi/domain/repositories/RestaurantRepository.java
+++ src/main/java/br/com/okfoodsapi/domain/repositories/RestaurantRepository.java
@@ -1,5 +1,8 @@
 package br.com.okfoodsapi.domain.repositories;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,7 @@ import br.com.okfoodsapi.domain.models.Restaurant;
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 	
+	List<Restaurant> findByTaxShippingBetween(BigDecimal taxInit, BigDecimal taxEnd);
+	
+	List<Restaurant> findByNameContainingAndCuisineId(String name, long cuisineId);
 }
