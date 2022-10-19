661d4414c8ad70eacaecc12951aa59a56ae8e37c
Lucas Rodrigues Veras <lucas.veras01@gmail.com>
2022-10-19 17:40:30 -0300

    Criteria API

--- src/main/java/br/com/okfoodsapi/infrastructure/repository/RestaurantRepositoryImpl.java
+++ src/main/java/br/com/okfoodsapi/infrastructure/repository/RestaurantRepositoryImpl.java
@@ -1,12 +1,19 @@
 package br.com.okfoodsapi.infrastructure.repository;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import br.com.okfoodsapi.domain.models.Restaurant;
 
@@ -19,13 +26,30 @@ public class RestaurantRepositoryImpl {
 	public List<Restaurant> find(String name, BigDecimal taxShippingInit,
 			BigDecimal taxShippingEnd){
 		
-		var jpql = "from Restaurant where name like :name "
-				+ "and taxShipping between :taxShippingInit and :taxShippingEnd";
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		
-		return manager.createQuery(jpql, Restaurant.class)
-				.setParameter("name", "%" + name + "%")
-				.setParameter("taxShippingInit", taxShippingInit)
-				.setParameter("taxShippingEnd", taxShippingEnd)
-				.getResultList();
+		CriteriaQuery<Restaurant> criteria = builder.createQuery(Restaurant.class);
+		Root<Restaurant> root = criteria.from(Restaurant.class); //from Restaurant
+		
+		var predicates = new ArrayList<Predicate>();
+		
+		if (StringUtils.hasText(name)) {
+			predicates.add(builder.like(root.get("name"), "%" + name + "%"));
+		}
+		
+		if (taxShippingInit != null) {
+			predicates.add(builder
+					.greaterThanOrEqualTo(root.get("taxShipping"), taxShippingInit));
+		}
+		
+		if (taxShippingEnd != null) {
+			predicates
+				.add(builder.lessThanOrEqualTo(root.get("taxShipping"), taxShippingEnd));
+		}
+		
+		criteria.where(predicates.toArray(new Predicate[0]));
+		
+		TypedQuery<Restaurant> query = manager.createQuery(criteria);
+		return query.getResultList();
 	};
 }
